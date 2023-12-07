4c990c94b02bd5ece42d12c06d5b61a65c1bc1b3
Szymon Półtorak <szymonpotorak@gmail.com>
2023-12-07 08:10:39 +0100

    Try

--- mwo-backend/src/main/java/razepl/dev/mwobackend/config/selenium/RegisterPage.java
+++ mwo-backend/src/main/java/razepl/dev/mwobackend/config/selenium/RegisterPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-@Accessors(fluent = true)
 @Getter
 public class RegisterPage {
 
